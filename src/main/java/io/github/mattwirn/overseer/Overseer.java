f87105f4c13ee9a97e0a2e0f45c2219e8a55cd21
Matthew Wirnowski <119266161+mattwirn@users.noreply.github.com>
2023-11-18 00:01:03 -0500

    Initial commit

---
+++ src/main/java/io/github/mattwirn/overseer/Overseer.java
@@ -0,0 +1,17 @@
+package io.github.mattwirn.overseer;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Overseer extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// TODO Insert logic to be performed when the plugin is enabled
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Insert logic to be performed when the plugin is disabled
+	}
+	
+	
+}
